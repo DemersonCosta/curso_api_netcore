7879d482c76deea1576bcf6ad9024a7739a6c3c9
demerson.ferreira <demerson.ferreira@digita.com.br>
2019-09-26 11:47:04 -0300

    metodo de logar

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,6 +1,8 @@
 using Api.Data.Context;
+using Api.Data.Implementations;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,8 +13,13 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+
+
             serviceCollection.AddDbContext<MyContext>(
             options => options.UseMySql("Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=32358115")
+            //options => options.UseSqlServer("Server=127.0.0.1,1433;Database=dbAPI;User Id=demerson;Password=Itachi%100")
+
             );
         }
 
--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -9,8 +9,11 @@ namespace Api.Data.Context
         {
             //Usado para Criar as Migrações
             var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=32358115";
+            //var connectionString = "Server=127.0.0.1,1433;Database=dbAPI;User Id=demerson;Password=Itachi%100";
+
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
+            //optionsBuilder.UseSqlServer(connectionString);
             return new MyContext(optionsBuilder.Options);
         }
 
